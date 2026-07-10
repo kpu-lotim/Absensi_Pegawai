a3635765c638b1db56b993f712cb07ebf4d16faa
kpu-lotim <majikongo@gmail.com>
2026-07-10 20:36:24 +0800

    Refactor service worker for installation and activation
    Removed cache deletion code and added skipWaiting and claim functionality.

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-// Ganti sementara dengan kode ini untuk membuang semua cache
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => caches.delete(cache))
-            );
-        })
-    );
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  clients.claim();
 });
